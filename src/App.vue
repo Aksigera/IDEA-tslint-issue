43313290402108ba38cc65059dac7da3eea8764d
Georgii Aksenov <georgii.aksenov@artin.cz>
2019-08-12 11:42:31 +0200

    go to App.vue to see the error

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,32 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
   </div>
 </template>
 
-<script>
+<script lang="ts">
+import Vue from 'vue';
 import HelloWorld from './components/HelloWorld.vue';
+import { Foo } from '@/modules/foo';
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld,
-  },
-};
+export default Vue.extend({
+	name: 'app',
+	components: {
+		HelloWorld
+	}, data() {
+		return { Foo }
+	}
+});
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
